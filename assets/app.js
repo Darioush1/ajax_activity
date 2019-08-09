14674644fea9e7fd9effd92a2876af3a78458767
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-09 01:17:46 -0700

    the for loop should be working but its not

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,25 @@
-var rugby = ['Sebastien Chabal', 'Jonah Lomu', 'Alesana Tuilagi', 'Manu Tuilagi', 'Ma\'a Nonu', 'Richie McCaw', 'Perry Baker', 'Henry Tuilagi','Sonny Bill Williams', 'Siya Kolisi', 'Faf de Klerk', 'Allan Alaalatoa', 'Nick Cummins']
+var rugby = ['Sebastien Chabal', 'Jonah Lomu', 'Alesana Tuilagi', 'Manu Tuilagi', 'Ma\'a Nonu', 'Richie McCaw', 'Perry Baker', 'Henry Tuilagi', 'Sonny Bill Williams', 'Siya Kolisi', 'Faf de Klerk', 'Allan Alaalatoa', 'Nick Cummins'];
+
+var buttonText = '';
 var searchText = '';
 var searchButton = document.getElementById('search');
 
-function addButton () {
-    document.getElementsByClassName('searchBox').append("<button id='gifButton'>yo</button>")
+function getButtons() {
+    for (i = 0; i < rugby.length; i++) {
+
+        console.log(rugby);
+        let gifButtons = "<button id='gifButton'></button>";
+        document.getElementsByClassName('Box').append(gifButtons)
+
+    }
+}
+
+    
+
 };
 
-searchButton.addEventListener('click', addButton())
+//searchButton.addEventListener('click', addButton())
+
+window.onload=function () {
+  getButtons();
+}
