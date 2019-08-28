dbba707e61e97db322367b9e8120a1c476fb3228
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-08-28 08:10:57 -0700

    new button text added and gifs now append

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
-var rugby = ["David_Fincher", "Spiderverse", "Bong JoonHo", "Wes_Anderson", "Jon_Favreau", "Dennis_Villeneauve", "Christopher_Nolan", "Patty_Jenkins", "Hayao_Miyazaki", "Paul_Thomas_Anderson", "Martin_Scorsese", "Kathryn_Bigelow", "Guy_Ritchie"];
+var rugby = ["David_Fincher", "Spiderverse", "Bong_JoonHo", "Wes_Anderson", "Jon_Favreau", "Edgar_Wright", "Christopher_Nolan", "Patty_Jenkins", "Hayao_Miyazaki", "Paul_Thomas_Anderson", "Martin_Scorsese", "Kathryn_Bigelow", "Guy_Ritchie"];
 
 var buttonText = '';
 var searchText = '';
 var searchText1 = '&q=' + searchText;
-var searchButton = document.getElementById('search');
+var newText = '';
 var giphyLink = "http://api.giphy.com/v1/gifs/search?&q=";
 var apiKey = "&api_key=iBag6lGxhBwV0gHDqNYT40XlbhPwKaoO&limit=10";
 var gifButtons;
@@ -68,7 +68,45 @@ function getButtons() {
   }
 }
 
-    //this adds the class gifButtons to the og object so we can add things to it in java/css
+
+
+  $('#search').on('click', function () {
+    
+    newText = $('#inputText').val();
+
+    var newButton = $('<button/>', {
+      text: newText,
+      class: 'gifButtons',
+      click: function () {
+        let searchText = newText;
+        var queryURL = giphyLink + searchText + apiKey;
+        // console.log('search text: ' + searchText)
+        // console.log(queryURL);
+
+        $.ajax({
+          url: queryURL,
+          method: 'GET'
+        }).then(function (response) {
+          //ask the ta's about this
+          var results = response.data;
+          // console.log('outside loop' + response)
+          for (i = 0; i < rugby.length; i++) {
+            console.log(results[0].images.fixed_height.url)
+
+            var gifDiv = $('<div>');
+            //var p = $('<p>').text(results.embed_url);
+            var gif = $('<img>');
+            gif = $('<img/>').attr('src', results[i].images.fixed_height.url);
+            gif.appendTo('.gifsHere')
+            // console.log('inside loop ' + response)
+
+          }
+        });
+      }
+    });
+    $('.box').append(newButton);
+  })
+
 
 
 
